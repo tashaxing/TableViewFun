e81b53543de9c14adf7487313086e747e59a9751
Ethan He <tashaxing123@163.com>
2016-10-10 16:49:41 +0800

    finish

--- TableViewFun/ViewController.m
+++ TableViewFun/ViewController.m
@@ -26,17 +26,24 @@
     sectionTitles = [[NSArray alloc] initWithObjects:
                            @"A",@"B",@"C",@"D",@"E",@"F",@"G",@"H", nil];
     contentsArray = [[NSArray alloc] initWithObjects:
-                           @[@"A1",@"A2",@"A3", @"A4"],
-                           @[@"B1",@"B2",@"B3",@"B4",@"B5"],
-                           @[@"C1",@"C2",@"方大同",@"芳芳",@"范伟"],
-                           @[@"郭靖",@"郭美美",@"过儿",@"过山车"],
-                           @[@"何仙姑",@"和珅",@"郝歌",@"好人"],
-                           @[@"妈妈",@"毛毛"],
-                           @[@"孙中山",@"沈冰",@"婶婶"],
-                           @[@"涛涛",@"淘宝",@"套娃"],
-                           @[@"小二",@"夏紫薇",@"许巍",@"许晴"],
-                           @[@"周恩来",@"周杰伦",@"张柏芝",@"张大仙"],nil];
+                           @[@"A1",@"A2",@"A3", @"A4", @"XX", @"XX", @"XX", @"XX", @"XX", @"XX", @"XX", @"XX"],
+                           @[@"B1",@"B2",@"B3",@"B4",@"B5", @"XX", @"XX", @"XX", @"XX", @"XX", @"XX", @"XX", @"XX"],
+                           @[@"C1",@"C2",@"C3",@"C4",@"C5", @"XX", @"XX", @"XX", @"XX", @"XX", @"XX", @"XX", @"XX"],
+                           @[@"D1",@"D2",@"D3",@"D4", @"XX", @"XX", @"XX", @"XX", @"XX", @"XX", @"XX", @"XX", @"XX"],
+                           @[@"E1",@"E2",@"E3",@"E4", @"XX", @"XX", @"XX", @"XX", @"XX", @"XX", @"XX", @"XX"],
+                           @[@"F1",@"F2", @"XX", @"XX", @"XX", @"XX"],
+                           @[@"G1",@"G2",@"G3", @"XX", @"XX"],
+                           @[@"H1",@"H2",@"H3", @"XX", @"XX"],
+                           nil];
+    
+    myTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 50, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
+    
+    myTableView.sectionIndexColor = [UIColor purpleColor]; // 右侧索引的颜色
+    myTableView.delegate = self;
+    myTableView.dataSource = self;
+    [self.view addSubview:myTableView];
 }
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -47,12 +54,101 @@
 #pragma mark - tableview delegate
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    
+    return sectionTitles.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
+    return [contentsArray[section] count];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 20;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    static NSString *headerViewId = @"headerViewId";
+    UITableViewHeaderFooterView *headerView = [tableView dequeueReusableHeaderFooterViewWithIdentifier:headerViewId];
+    if (!headerView)
+    {
+        headerView = [[UITableViewHeaderFooterView alloc] initWithReuseIdentifier:headerViewId];
+    }
+        
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, myTableView.frame.size.width, 20)];
+    [titleLabel setBackgroundColor:[UIColor greenColor]];
+    titleLabel.text = sectionTitles[section];
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    [headerView addSubview:titleLabel];
     
+    return headerView;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *detailIndicated = @"tableCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:detailIndicated];
+    
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:detailIndicated];
+    }
+    cell.textLabel.text = contentsArray[indexPath.section][indexPath.row];
+    
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // 取消选中效果
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    // 获取点击行的cell
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    
+    // 如果cell已经被标记
+    if (cell.accessoryType == UITableViewCellAccessoryCheckmark)
+    {
+        // 取消标记
+        cell.accessoryType  = UITableViewCellAccessoryNone;
+    }
+    else
+    {
+        // 如果cell未标记就标记
+        cell.accessoryType  = UITableViewCellAccessoryCheckmark;
+    }
+    
+}
+
+// 右侧索引
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return [sectionTitles objectAtIndex:section];
+}
+// 索引目录
+- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView
+{
+    return sectionTitles;
+}
+
+// 点击目录
+- (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index
+{
+    NSLog(@"clicked section: %ld", index);
+    // 获取所点目录对应的indexPath值
+    NSIndexPath *selectIndexPath = [NSIndexPath indexPathForRow:0 inSection:index];
+    
+    // 让table滚动到对应的indexPath位置
+    [tableView scrollToRowAtIndexPath:selectIndexPath atScrollPosition:UITableViewScrollPositionBottom animated:YES];
+    
+    return index;
 }
 
 @end
